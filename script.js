af5b4b7d0a4013641908c5937385175069ebc847
Quentin Geerts <quentin.geerts@cogntic.be>
2026-04-01 16:52:52 +0200

    fix: comportement boutton #3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Hello les loustiques !");
 
 const btn = document.getElementById("btn");
+const btn2 = document.getElementById("btn2");
 
 btn.addEventListener("click", () => alert("T'as cliqué sur moi !!!!"));
+btn2.addEventListener("click", () => alert("T'es content maintenant ??!"));
